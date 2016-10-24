bbece3ff99cd66f1ee3b036961324daefaa21626
jkaminallen <kaminallenj@gmail.com>
2016-10-24 11:40:40 -0400

    Added background color

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ require 'httparty'
 require 'pry'
 class MoviesController < ApplicationController
   def index
-    response = HTTParty.get("https://api.themoviedb.org/3/discover/movie?api_key=6f40eadd5aee54bd20363edc6206ef54&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1000&vote_average.gte=7")
+    response = HTTParty.get("https://api.themoviedb.org/3/discover/movie?api_key=6f40eadd5aee54bd20363edc6206ef54&language=en-US&include_adult=false&include_video=false&vote_average.gte=7")
     movie = response["results"].sample
       @title = movie["original_title"]
       @poster = "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}"
